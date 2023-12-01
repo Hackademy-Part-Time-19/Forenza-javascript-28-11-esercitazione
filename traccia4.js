1f7bf6c052f7b3e8ec3eed569fbec16d6c71df4f
fabio <fabio.forenza@gmail.com>
2023-12-01 10:38:22 +0100

    fine ersercizio

--- traccia4.js
+++ traccia4.js
@@ -1,6 +1,30 @@
-function updateDate() {
-  let date = new Date()
-  let formatDate = date.toLocaleDateString()
-  var dataElement = document.querySelector('.data')
-  dataElement.textContent = 'Data: ' + formatDate
+function inserisciCards() {
+  let titolo = document.getElementById('insTitolo').value
+  let paragrafo = document.getElementById('paragrafo').value
+  if (titolo.trim() === '' || paragrafo.trim() === '') {
+    alert('Assicurati di compilare tutti i campi.')
+    return
+  }
+  let date = new Date().toLocaleDateString()
+  let card = document.createElement('div')
+  card.className = 'card'
+  document.getElementById('innerContainerCards').innerHTML += `
+  <div class="cards">
+  <div class="tit-art">
+    <h1 id="title" >${titolo}</h1>
+  </div>
+  <div class="par-art">
+    <p id=""text-article>${paragrafo}</p>
+  </div>
+  <div class="data">
+    <p class="giorno">Pubblicato il  ${date}</p>
+  </div>
+  </div>
+
+  `
+  let innerContainer = document.getElementById('innerContainerCards')
+  innerContainer.appendChild(card)
+
+  document.getElementById('insTitolo').value = ''
+  document.getElementById('paragrafo').value = ''
 }
